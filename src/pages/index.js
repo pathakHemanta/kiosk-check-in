e1efe69daa3a19b46b5162c99cb70f640bc279ab
pathakHemanta <pathakh@warhawks.ulm.edu>
2023-09-10 11:12:35 -0500

    Project Setup

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <div>First Name Last Name</div>
+      <div>Host</div>
+      <div>Reason for visit</div>
+      <div>SUBMIT</div>
+    </main>
+  );
+}
