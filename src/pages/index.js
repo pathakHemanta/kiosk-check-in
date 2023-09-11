ebb03cbe06c634bb571a8995a60789215d175ac3
pathakHemanta <pathakh@warhawks.ulm.edu>
2023-09-11 13:14:45 -0500

    Created the first phase of UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,47 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [name, setName] = useState("None");
+  const [serverName, setServerName] = useState("None");
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setServerName(name);
+  }
+
   return (
-    <main>
-      <div>First Name Last Name</div>
-      <div>Host</div>
-      <div>Reason for visit</div>
-      <div>SUBMIT</div>
-    </main>
+    <div className="h-screen flex flex-col justify-center items-center bg-red-400">
+      <form
+        className="flex flex-col items-center max-w-sm"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          value={name}
+          name="name"
+          placeholder="Name"
+          className="input input-bordered input-primary w-full h-8 text-sm spacing"
+          onChange={handleNameChange}
+        />
+        <select className="w-full rounded-md spacing">
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </select>
+        <input
+          type="text"
+          placeholder="Reason"
+          className="input input-bordered input-primary w-full text-sm spacing"
+        />
+        <button className="btn btn-primary spacing" type="submit">
+          SUBMIT
+        </button>
+      </form>
+      <div className="bg-yellow-300 rounded-md p-auto mt-80">{serverName}</div>
+    </div>
   );
 }
---
+++ src/pages/test.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Test = () => {
+  return (
+    <div className="h-screen flex flex-col justify-center items-center w-[500px] m-auto bg-green-500 px-3">
+      <div className="bg-red-500 w-full my-3">Div 1</div>
+      <div className="bg-pink-500 ">Div 2</div>
+    </div>
+  );
+};
+
+export default Test;
